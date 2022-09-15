5375344c69e72eb09c6f879f2fe4ad88ebec1bab
gojaguerra <jguerra1968@gmail.com>
2022-09-15 15:34:06 -0300

    limpieza de codigo

--- src/Containers/ItemDetailContainer/ItemDetailContainer.js
+++ src/Containers/ItemDetailContainer/ItemDetailContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from "../../logo.svg";
-import getFetch from '../../Data/data'
+// import logo from "../../logo.svg";
+// import getFetch from '../../Data/data'
 import { useState, useEffect } from 'react'
 import ItemDetail from '../../components/ItemDetail/ItemDetail'
 import { useParams } from 'react-router-dom';
@@ -9,16 +9,13 @@ import { Data } from '../../Data/data';
 const ItemDetailContainer = ( {setItems} ) => {
   const { id } = useParams();
   const[data, setData] = useState([]);
-  const[loading, setLoading] = useState(true);
+  // const[loading, setLoading] = useState(true);
 
 const getProduct = () => {
 
   const prodFiltro = Data.find((el) => el.id === id);
   setData(prodFiltro);
   
- /*  const prodFiltro = Data.filter((el) => el.id === id);
-  setData(prodFiltro[0]); */
-
  };
 
 useEffect(() => {
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
-import { Button } from 'react-bootstrap'
-import Form from 'react-bootstrap/Form';
+import React from 'react'
+// import { Button } from 'react-bootstrap'
+// import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 
 const Item = ({data}) => {
     
-    const[cantidad,setCantidad]=useState(1)
+/*     const[cantidad,setCantidad]=useState(1)
 
     const increment=()=>{
         setCantidad(actualValor=>actualValor+1)
@@ -13,7 +13,7 @@ const Item = ({data}) => {
 
     const decrement=()=>{
         setCantidad(actualValor=>actualValor-1)
-    }
+    } */
   
   return (
     <>
@@ -23,10 +23,6 @@ const Item = ({data}) => {
         <Card.Title>{data.name}</Card.Title>
         <Card.Text>{data.description}</Card.Text>
         <Card.Text>Precio: ${data.price}</Card.Text>
-{/*         <Button className='btn bg-dark' onClick={increment} disabled={cantidad>=data.stock} >+</Button>
-        <Form.Text className='texto1'>{cantidad}</Form.Text>
-        <Button className='btn bg-dark' onClick={decrement} disabled={cantidad<1} >-</Button>
-        <Button className='btn-primary' onClick={()=>{console.log(`agregaste ${cantidad} producto/s`)}}>Agregar</Button> */}
       </Card.Body>
       </Card>
     </>
